249339ef2dba43db46df6e77d3841758246de24d
rafi993 <rafi993@protonmail.com>
2020-06-20 14:05:59 +0530

    Styling labels

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const addLabel = (element) => {
+  const label = element.querySelector("font font");
+  if ((label.innerText || "").length > 0) {
+    label.style.backgroundColor = label.color;
+    label.classList.add("label");
+  }
+};
+
 try {
   const searchButton = document.querySelector('input[name="nvp_site_web"');
   searchButton.value = "Web";
@@ -8,7 +16,7 @@ try {
   const menubar = document.getElementById("guser");
   const profile = document.createElement("div");
   profile.id = "profile";
-  profile.innerText = email;
+  profile.innerText = (email || "").split("@")[0];
   menubar.prepend(profile);
 
   const logout = document.createElement("a");
@@ -23,5 +31,19 @@ try {
     .querySelector("body > table:nth-child(15) > tbody > tr > td:nth-child(1)")
     .remove();
 
-  // document.getElementById("guser").remove();
+  // Styling read emails
+  Array.from(document.querySelectorAll("tr[bgcolor='#E8EEF7']")).forEach(
+    (element, index) => {
+      element.classList.add("read-email");
+      addLabel(element);
+    }
+  );
+
+  // Unread emails
+  Array.from(document.querySelectorAll("tr[bgcolor='#ffffff']")).forEach(
+    (element, index) => {
+      element.classList.add("unread-email");
+      addLabel(element);
+    }
+  );
 } catch (error) {}
